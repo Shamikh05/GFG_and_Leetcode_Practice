1f3715d5b19e0147f25680a37bd02ca1c43effff
Shamikh <54002765+Shamikh05@users.noreply.github.com>
2022-06-03 15:36:08 +0530

    Time: 633 ms (43.35%), Space: 147.9 MB (92.98%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -37,15 +37,6 @@ public:
     
     int sumRegion(int row1, int col1, int row2, int col2) {
         
-//         for(int i=0;i<2;++i) {
-            
-//             for(int j=0;j<1;++j) {
-                
-//                 cout << preSum[i][j] << " ";
-//             }
-//             cout << endl;
-//         }
-        
         int upperSum = 0;
         int upperRemSum = 0;
         
