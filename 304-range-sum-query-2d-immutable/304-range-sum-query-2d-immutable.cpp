90c81a907e3c81b69a1adfad30180b01c623d1af
Shamikh <54002765+Shamikh05@users.noreply.github.com>
2022-06-03 15:02:23 +0530

    Time: 476 ms (75.35%), Space: 147.9 MB (79.01%) - LeetHub

---
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -0,0 +1,77 @@
+class NumMatrix {
+private:
+    vector<vector<int> > preSum;
+public:
+    NumMatrix(vector<vector<int>>& matrix) {
+        
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        preSum = vector<vector<int> >(matrix.begin(), matrix.end());
+        
+        for(int i=0;i<m;++i) {
+            
+            for(int j=0;j<n;++j) {
+                
+                int upperSum = 0;
+                int upperRemSum = 0;
+                    
+                int leftSum = 0;
+                
+                if(i)
+                    upperSum = preSum[i-1][j];
+                
+                if(i and j)
+                    upperRemSum = preSum[i-1][j-1];
+                    
+                int netUpperSum = upperSum - upperRemSum;
+                
+                if(j) 
+                    leftSum = preSum[i][j-1];
+                
+                
+                preSum[i][j] += leftSum + netUpperSum;
+            }
+        }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        
+//         for(int i=0;i<2;++i) {
+            
+//             for(int j=0;j<1;++j) {
+                
+//                 cout << preSum[i][j] << " ";
+//             }
+//             cout << endl;
+//         }
+        
+        int upperSum = 0;
+        int upperRemSum = 0;
+        
+        if(row1) 
+            upperSum = preSum[row1-1][col2];
+        
+        if(row1 and col1)
+            upperRemSum = preSum[row1-1][col1-1];
+        
+        
+        int upperDiff = upperSum - upperRemSum;
+        
+        int leftSum = 0;
+        
+        if(col1)
+            leftSum = preSum[row2][col1-1];
+        
+        
+        int sumRegion = preSum[row2][col2] - upperDiff - leftSum;
+        
+        return sumRegion;
+    }
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
